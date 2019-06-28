42e2eebcf644ac00520807efeffb142f2ab555e3
waconde <2523890679@qq.com>
2019-06-28 09:53:39 +0800

    Update Item.java

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -3,7 +3,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 // 22222222222222222222222222222222222
-
+// 11111111111111111111111111111111111
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
